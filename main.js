2110384cd3ff90cf0a4bfb0ef4477f3c5286d5c7
Dom Bartenope <dominickbartenope@yahoo.com>
2021-09-23 17:53:05 -0400

    Initial commit, added logic to GCF calc

---
+++ main.js
@@ -0,0 +1,37 @@
+
+const firstInput = document.querySelector('.a-input');
+const secondInput = document.querySelector('.b-input');
+const button = document.querySelector('button');
+const resultText = document.querySelector('.result');
+
+const findTheHCF = (a, b) => {
+    let result;
+    //check if params are equal, and if so, set result to a and b
+    if (a === b) {
+        result = b;
+        console.log(result);
+    }
+    //Check which number is lower, because the hcf 
+    //cannot be larger than the lowest number
+    if(a > b) {
+        for (let i = b; i > 0; i--) {
+            if (a % i === 0 && b % i === 0) {
+                result = i;
+                break;
+            }
+        }
+    } else {
+        for (let i = a; i > 0; i--) {
+            if (a % i === 0 && b % i === 0) {
+                result = i;
+                break;
+            }
+        }
+    }
+    
+    return result;
+}
+
+button.addEventListener('click', () => {
+    resultText.textContent = findTheHCF(firstInput.value, secondInput.value);
+})
